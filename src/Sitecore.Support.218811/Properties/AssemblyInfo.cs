aac4164f69a709d61beeadc08fd53b310bb86188
DK\obr <olegbraginskyy@sitecore.net>
2018-04-25 14:14:17 +0300

    Rewriting the link for the internal items while using OpenLink

---
+++ src/Sitecore.Support.218811/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.218811")]
+[assembly: AssemblyProduct("Sitecore.Support.218811")]
+[assembly: ComVisible(false)]
